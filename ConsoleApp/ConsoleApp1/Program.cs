aa747662693776c776eee7cea1a73f80894dbb9f
Fred Daniel Varelo <fdvarelo@gmail.com>
2022-07-15 11:03:21 -0300

    Iniciando a resolução com a divisão das strings

--- ConsoleApp/ConsoleApp1/Program.cs
+++ ConsoleApp/ConsoleApp1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantidade de produtos: ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtd; i++)
+            {
+                string[] vet = Console.ReadLine().Split(',');
+                double preco = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
