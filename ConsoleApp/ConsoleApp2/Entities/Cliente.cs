9b4b146cc36a25fa63402a15f359c73ca308d8c2
Fred Daniel Varelo <fdvarelo@gmail.com>
2022-07-15 16:35:50 -0300

    Utilizando Equals e GetHashCode em classes, e mais alguns testes

---
+++ ConsoleApp/ConsoleApp2/Entities/Cliente.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp2.Entities
+{
+    class Cliente
+    {
+        // Utilização de Equal e GetHashCode em classes.
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is Cliente)) // Verifica se não é um cliente.
+            {
+                return false;
+            }
+            Cliente cliente = obj as Cliente;
+            return Email.Equals(cliente.Email); // Compara o Email com o outro Email.
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode(); // Pega o HashCode do email.
+        }
+    }
+}
--- ConsoleApp/ConsoleApp2/Program.cs
+++ ConsoleApp/ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ConsoleApp2.Entities;
 
 namespace ConsoleApp2
 {
@@ -17,6 +18,30 @@ namespace ConsoleApp2
             // GetHashCode, por que motivo ã resposta não é 100% positiva:
             // Se o código de dois objetos for diferente, então os dois objetos são diferentes, porém,
             // se o código de dois objetos for igual, muito provavelmente os objetos são iguais(pode haver colisão) ←
+
+            Console.WriteLine("\nUtilizando Classe Cliente com Equal e GetHashCode:\n");
+
+            Cliente c = new Cliente { Nome = "Camila", Email = "camila@email.com" };
+            Cliente d = new Cliente { Nome = "Daniel", Email = "daniel@email.com" };
+
+            // O Equals e o GetHashCode so é possível fazer com classe por conta do override na classe Cliente.
+            Console.WriteLine("Equals Cliente (c) e (d): " + a.Equals(b));
+            Console.WriteLine("GetHashCode (c): " + c.GetHashCode());
+            Console.WriteLine("GetHashCode (d): " + d.GetHashCode());
+
+            // Outros testes.
+            int num1 = 1;
+            char numUm = '1';
+            string numUms = "1";
+
+            Console.WriteLine("\nTeste entre tipos diferentes int 1 / char '1' / string \"1\":\n\nEquals:");
+            Console.WriteLine("int e char: " + num1.Equals(numUm));
+            Console.WriteLine("int e string: " + num1.Equals(numUms));
+            Console.WriteLine("char e string: " + numUm.Equals(numUms));
+            Console.WriteLine("\nGetHashCode:");
+            Console.WriteLine("int: " + num1.GetHashCode());
+            Console.WriteLine("char: " + numUm.GetHashCode());
+            Console.WriteLine("string: " + numUms.GetHashCode());
         }
     }
 }
