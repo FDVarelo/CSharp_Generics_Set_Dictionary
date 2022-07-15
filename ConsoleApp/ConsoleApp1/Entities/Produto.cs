15d63e1c4ed7a0422bad2d6a7f410cd8702cf123
Fred Daniel Varelo <fdvarelo@gmail.com>
2022-07-15 11:09:04 -0300

    Criação da Entidade Produto

---
+++ ConsoleApp/ConsoleApp1/Entities/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1.Entities
+{
+    class Produto : IComparable
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Produto))
+            {
+                throw new ArgumentException("Erro na comparação: argumento não é um Produto!");
+            }
+            Produto produto = obj as Produto;
+            return Preco.CompareTo(produto.Preco);
+        }
+    }
+}
