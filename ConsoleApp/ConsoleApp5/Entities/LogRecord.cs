fb6deb42509ef1a97e90b478e63a27b57c49e5f6
Fred Daniel Varelo <fdvarelo@gmail.com>
2022-07-19 14:56:05 -0300

    Exercício finalizado

---
+++ ConsoleApp/ConsoleApp5/Entities/LogRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp5.Entities
+{
+    class LogRecord
+    {
+
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode(); // Retorna o nome/username, que ele ira buscar.
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is LogRecord)) // Se não por um LogRecord.
+            {
+                return false;
+            }
+
+            // Caso seja um LogRecord
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username); // Verifica se tem algum username cadastrado com aquele username.
+        }
+    }
+}
--- ConsoleApp/ConsoleApp5/Program.cs
+++ ConsoleApp/ConsoleApp5/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ConsoleApp5.Entities;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>(); // Cria uma entidade LogRecord com HashSet.
+
+            //Console.Write("Enter file full path: "); // Descomentar caso queir aque o usuário defina o caminho a ser escolhido.
+            //string path = Console.ReadLine();
+
+            string path = @"C:\temp\CSharp_Generics_Set_Dictionary\ConsoleApp\ConsoleApp5\texts\file.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path)) // Abrindo o arquivo.
+                {
+                    while (!sr.EndOfStream) // Enquanto não acabar o arquivo.
+                    {
+                        string[] line = sr.ReadLine().Split(' '); // Dividir o arquivo em 'name' e 'instant'.
+
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+
+                        set.Add(new LogRecord { Username = name, Instant = instant }); // Adiciona o novo LogRecord.
+                    }
+                    Console.WriteLine("Total users: " + set.Count); // Printa na tela a quantidade de usuários cadastrados em set.
+                }
+            }
+            catch (IOException e) // Caso não encontre o arquivo, ou de qualquer outro tipo de erro.
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
