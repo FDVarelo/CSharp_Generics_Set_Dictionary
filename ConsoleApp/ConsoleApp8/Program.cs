f49449d776f3e0cdd1493b5b4a4a8ec1684f65ad
Fred Daniel Varelo <fdvarelo@gmail.com>
2022-07-20 09:30:10 -0300

    Iniciando Exercício proposto final

---
+++ ConsoleApp/ConsoleApp8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Caso queira que o usuário decida o diretorio:
+            //Console.Write("Enter file full path: ");
+            //string path = Console.ReadLine();
+
+            // Commitar a linha abaixo caso queira que o usuário decida o diretorio.
+            string path = @"C:\temp\CSharp_Generics_Set_Dictionary\ConsoleApp\ConsoleApp8\texts\file.csv";
+
+            try
+            {
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("\nOcorreu um erro!\n" + e.Message);
+            }
+        }
+    }
+}
