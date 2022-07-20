4a51002dc660be23c1497f57a140ca45a2ec4dc6
Fred Daniel Varelo <fdvarelo@gmail.com>
2022-07-20 09:37:39 -0300

    Exercício finalizado

--- ConsoleApp/ConsoleApp8/Program.cs
+++ ConsoleApp/ConsoleApp8/Program.cs
@@ -15,6 +15,32 @@ namespace ConsoleApp8
 
             try
             {
+                // Abrindo arquivo
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] dados = sr.ReadLine().Split(',');
+                        string candidato = dados[0];
+                        int votos = int.Parse(dados[1]);
+
+                        if (dictionary.ContainsKey(candidato)) // Caso já tenha tal cantidato no dictionary.
+                        {
+                            dictionary[candidato] += votos; // Adiciona apenas os novos votos.
+                        }
+                        else
+                        {
+                            dictionary[candidato] = votos; // Adiciona os primeiros votos
+                        }
+                    }
+
+                    foreach(var item in dictionary)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
 
             }
             catch (IOException e)
